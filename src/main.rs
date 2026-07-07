8dbac552d3c995cb33545ea2b86dea7a664f5fe5
Adhidarma Hadiwinoto <adhisimon@tektrans.id>
2026-07-08 02:58:08 +0700

    Skeleton for main.rs using cli entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+mod master;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "Siloka Object Storage")]
+struct Args {
+    #[arg(short, long)]
+    mode: String, // "master", "worker", atau "storage"
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let args = Args::parse();
+
+    match args.mode.as_str() {
+        "master" => {
+            println!("Starting siloka-master...");
+            master::run();
+        }
+        _ => {
+            eprintln!("Unknown component to run: {}", args.mode);
+            std::process::exit(1);
+        }
+    }
 }
